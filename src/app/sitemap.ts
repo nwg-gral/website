b1b5876ce94c5970681287d99cddf15adb34db63
Stefan Probst <stefanprobst@posteo.de>
2024-01-14 22:01:33 +0100

    fix: clean up sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,12 +5,12 @@ import { baseUrl } from "~/config/app.config";
 import { locales } from "~/config/i18n.config";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-	const routes = ["/", "/activities", "/contact", "/networks", "/research", "/team"];
+	const routes = ["", "/activities", "/contact", "/networks", "/research", "/team"];
 
 	return locales.flatMap((locale) => {
 		return routes.map((pathname) => {
 			return {
-				url: String(createUrl({ baseUrl, pathname: `/${locale}/${pathname}` })),
+				url: String(createUrl({ baseUrl, pathname: `/${locale}${pathname}` })),
 				lastModified: new Date(),
 			};
 		});
